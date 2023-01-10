fd1c6d669bf79b25046e119180ad20bb2785975f
Ankita <priya.singhankita@gmail.com>
2023-01-10 10:12:45 +0530

    added function to shorten the URL

--- src/components/Shortening.js
+++ src/components/Shortening.js
@@ -1,8 +1,46 @@
 import bgMobile from "../images/bg-shorten-mobile.svg";
 import bgDesktop from "../images/bg-shorten-desktop.svg";
-import React from "react";
+import React, { useState } from "react";
 
 const Shortening = () => {
+  const [shortenedLinks, setShortenedLinks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = e.target.elements.url.value;
+    fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setShortenedLinks([...shortenedLinks, data.result.short_link]);
+      });
+    // console.log(shortenedLinks);
+
+    const ShortenedLinkCard = ({ link }) => {
+      const [copiedText, setCopiedText] = useState("");
+      return (
+        <div
+          key={link.short_link}
+          className="flex items-center justify-between shadow-md p-4 rounded-lg"
+        >
+          <div className="flex items-center justify-between w-full pr-4 font-semibold">
+            <div className="text-gray-600">{link.oiginal_link}</div>
+            <div className="text-cyan-600">{link.short_link}</div>
+          </div>
+          <button
+            className={`w-32 py-2 text-white rounded-lg font-semibold ${
+              copiedText ? "bg-gray-600" : "bg-gray-600"
+            }`}
+            onClick={() => {
+              navigator.clipboard.writeText(link.short_link);
+              setCopiedText(link.short_link);
+            }}
+          >
+            {copiedText ? "Copied" : "Copy"}
+          </button>
+        </div>
+      );
+    };
+  };
   return (
     <div>
       <section className="px-5 lg:max-w-5xl lg:mx-auto xl:max-w-6xl 2xl:px-0 shortener relative">
@@ -11,10 +49,11 @@ const Shortening = () => {
           <img src={bgMobile} alt="" />
         </picture>
 
-        <form className="form">
+        <form onSubmit={handleSubmit} className="form">
           <div className="flex flex-col md:flex-row">
             <input
               type="url"
+              name="url"
               placeholder="Shorten a link here"
               className="w-full py-2 px-6 rounded-lg md:mb-0"
             />
